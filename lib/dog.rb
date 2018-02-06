98d0c8f8765249b413151b4aeb820e8412799e37
Megan Collier <meganraec@live.com>
2018-02-06 02:15:49 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
     @name = name
   end
 
-  def name=(name)
+  def name
     @name
   end
 
@@ -11,7 +11,7 @@ class Dog
     @breed = breed
   end
 
-  def breed=(breed)
+  def breed
     @breed
   end
 end
